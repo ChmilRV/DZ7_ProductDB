7f49eceec083cfe1908136fd2d7d5c32d0d3ccac
Chmil Ruslan <chmilrv@users.noreply.github.com>
2021-10-04 18:54:33 +0300

    Добавьте файлы проекта.

---
+++ DZ7_ProductDB/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DZ7_ProductDB.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
---
+++ DZ7_ProductDB/Models/ProductContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DZ7_ProductDB.Models
+{
+	public class ProductContext
+	{
+	}
+}
