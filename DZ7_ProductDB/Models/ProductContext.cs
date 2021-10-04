6b9d8b2642aaaa683a74223b547318e6f3d7b5ea
Chmil Ruslan <chmilrv@users.noreply.github.com>
2021-10-04 19:05:12 +0300

    1905

--- DZ7_ProductDB/Models/ProductContext.cs
+++ DZ7_ProductDB/Models/ProductContext.cs
@@ -1,11 +1,23 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace DZ7_ProductDB.Models
 {
-	public class ProductContext
+	public partial class ProductContext: DbContext
 	{
+		public ProductContext()
+		{
+		}
+		public ProductContext(DbContextOptions<ProductContext> options)
+			:base(options)
+		{
+		}
+
+		public virtual DbSet<Product> Products { get; set; }
+
+
 	}
 }
